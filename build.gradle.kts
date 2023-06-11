432709af665031b5b100fcd423e22ce50bd52da3
booky10 <boooky10@gmail.com>
2023-06-11 03:09:49 +0200

    Bump discord4j version to v3.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 val configurateVersion = "4.1.2"
-val discord4jVersion = "3.2.3"
+val discord4jVersion = "3.2.4"
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
