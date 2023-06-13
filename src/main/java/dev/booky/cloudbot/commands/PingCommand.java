a00ae9d72c79b01321f464752cd76c1e2680eb0c
booky10 <boooky10@gmail.com>
2023-06-13 21:17:27 +0200

    Revert "Change ping command"
    This reverts commit 5712a2745e7602b7fcac7951a1f23e34765b7423.

--- src/main/java/dev/booky/cloudbot/commands/PingCommand.java
+++ src/main/java/dev/booky/cloudbot/commands/PingCommand.java
@@ -10,8 +10,6 @@ import discord4j.discordjson.json.ImmutableApplicationCommandRequest;
 import discord4j.rest.util.Color;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Map;
 
 public final class PingCommand extends AbstractBotCommand {
@@ -30,21 +28,21 @@ public final class PingCommand extends AbstractBotCommand {
 
     @Override
     public Mono<Void> run(ChatInputInteractionEvent event, User user, Translator i18n) {
+        long start = System.currentTimeMillis();
         return event.reply()
                 .withEphemeral(true)
                 .withEmbeds(EmbedCreateSpec.builder()
                         .color(Color.CYAN)
-                        .description(i18n.apply("command.ping.sent", user.getMention()))
-                        .build())
-                .flatMap($ -> event.editReply().withEmbeds(EmbedCreateSpec.builder()
-                        .color(Color.CYAN)
                         .description(i18n.apply("command.ping.waiting", user.getMention()))
-                        .build()))
-                .flatMap(message -> event.editReply().withEmbeds(EmbedCreateSpec.builder()
-                        .color(Color.CYAN)
-                        .description(i18n.apply("command.ping.result", user.getMention(),
-                                Duration.between(message.getEditedTimestamp().orElseThrow(), Instant.now()).toMillis()))
-                        .build()))
+                        .build()).then()
+                .and(Mono.defer(() -> {
+                    long ping = System.currentTimeMillis() - start;
+                    return event.editReply()
+                            .withEmbeds(EmbedCreateSpec.builder()
+                                    .color(Color.CYAN)
+                                    .description(i18n.apply("command.ping.result", user.getMention(), ping))
+                                    .build());
+                }))
                 .then();
     }
 }
